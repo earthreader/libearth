81bc9f317101f5ef7f7fcabfd3fb1677b8a91e38
Hong Minhee <minhee@dahlia.kr>
2013-07-27 04:18:07 +0900

    Use tagged offsets instead of hard-coded offsets

--- tests/schema_test.py
+++ tests/schema_test.py
@@ -21,12 +21,17 @@ class TestDoc(DocumentElement):
 
 def string_chunks(consume_log, *chunks):
     """Iterate the given chunks of a text with logging consumed offsets
-    to test laziness of the parser.
+    to test laziness of the parser.  If an argument is a list (that consists
+    of a string) it's treated as offset tagging.
 
     """
+    size = len(chunks)
     for i, chunk in enumerate(chunks):
+        if type(chunk) is list:
+            continue
         chunk = text(chunk)
-        consume_log.append((i + 1, chunk))
+        if size > i + 1 and type(chunks[i + 1]) is list:
+            consume_log.append(chunks[i + 1][0])
         yield chunk
 
 
@@ -35,66 +40,67 @@ def fx_test_doc():
     consume_log = []
     xml = string_chunks(
         consume_log,
-        '<test>', '\n',
-        '\t', '<title>', 'Title ', 'test', '</title>', '\n',
-        '\t', '<multi>', 'a', '</multi>', '\n',
-        '\t', '<content>', 'Content', ' test', '</content>', '\n',
-        '\t', '<multi>', 'b', '</multi>', '\n',
-        '\t', '<multi>', 'c', '</multi>', '\n',
-        '</test>', '\n'
+        '<test>', ['TEST_START'], '\n',
+        '\t', '<title>', 'Title ', 'test', '</title>', ['TITLE_CLOSE'], '\n',
+        '\t', '<multi>', 'a', '</multi>', ['MULTI_1_CLOSE'], '\n',
+        '\t', '<content>', 'Content', ' test',
+        '</content>', ['CONTENT_CLOSE'], '\n',
+        '\t', '<multi>', 'b', '</multi>', ['MULTI_2_CLOSE'], '\n',
+        '\t', '<multi>', 'c', '</multi>', ['MULTI_3_CLOSE'], '\n',
+        '</test>', ['TEST_CLOSE'], '\n'
     )
     return TestDoc(xml), consume_log
 
 
 def test_document_parse(fx_test_doc):
     doc, consume_log = fx_test_doc
-    assert consume_log[-1][0] == 1
+    assert consume_log[-1] == 'TEST_START'
     assert doc.title.value == 'Title test'
-    assert consume_log[-1][0] == 7
+    assert consume_log[-1] == 'TITLE_CLOSE'
     assert doc.content.value == 'Content test'
-    assert consume_log[-1][0] == 18
+    assert consume_log[-1] == 'CONTENT_CLOSE'
     assert isinstance(doc.multi, collections.Sequence)
 
 
 def test_multiple_child_iter(fx_test_doc):
     doc, consume_log = fx_test_doc
     it = iter(doc.multi)
-    assert consume_log[-1][0] == 1
+    assert consume_log[-1] == 'TEST_START'
     assert next(it).value == 'a'
-    assert consume_log[-1][0] == 12
+    assert consume_log[-1] == 'MULTI_1_CLOSE'
     assert next(it).value == 'b'
-    assert consume_log[-1][0] == 23
+    assert consume_log[-1] == 'MULTI_2_CLOSE'
     assert next(it).value == 'c'
-    assert consume_log[-1][0] == 28
+    assert consume_log[-1] == 'MULTI_3_CLOSE'
     with raises(StopIteration):
         next(it)
-    assert consume_log[-1][0] == 30
+    assert consume_log[-1] == 'TEST_CLOSE'
 
 
 def test_multiple_child_len(fx_test_doc):
     doc, consume_log = fx_test_doc
-    assert consume_log[-1][0] == 1
+    assert consume_log[-1] == 'TEST_START'
     assert len(doc.multi) == 3
-    assert consume_log[-1][0] == 30
+    assert consume_log[-1] == 'TEST_CLOSE'
 
 
 def test_multiple_child_getitem(fx_test_doc):
     doc, consume_log = fx_test_doc
-    assert consume_log[-1][0] == 1
+    assert consume_log[-1] == 'TEST_START'
     doc.multi[0].value == 'a'
-    assert consume_log[-1][0] == 12
+    assert consume_log[-1] == 'MULTI_1_CLOSE'
     doc.multi[1].value == 'b'
-    assert consume_log[-1][0] == 23
+    assert consume_log[-1] == 'MULTI_2_CLOSE'
     doc.multi[2].value == 'c'
-    assert consume_log[-1][0] == 28
+    assert consume_log[-1] == 'MULTI_3_CLOSE'
 
 
 def test_multiple_child_getitem_from_last(fx_test_doc):
     doc, consume_log = fx_test_doc
-    assert consume_log[-1][0] == 1
+    assert consume_log[-1] == 'TEST_START'
     doc.multi[2].value == 'c'
-    assert consume_log[-1][0] == 28
+    assert consume_log[-1] == 'MULTI_3_CLOSE'
     doc.multi[1].value == 'b'
-    assert consume_log[-1][0] == 28
+    assert consume_log[-1] == 'MULTI_3_CLOSE'
     doc.multi[0].value == 'a'
-    assert consume_log[-1][0] == 28
+    assert consume_log[-1] == 'MULTI_3_CLOSE'
