8bcdd9f6f3a003192dc73d1f3a68ce244e1f4476
Choi Bochul <vio.bo94@gmail.com>
2013-08-05 17:10:41 +0900

    Make Crawler class and basic methods

--- libearth/crawler.py
+++ libearth/crawler.py
@@ -2,3 +2,28 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+
+import re
+import os
+import urllib2
+
+
+class Crawler(object):
+
+    def crawl(self, url):
+        request = urllib2.Request(url)
+        f = urllib2.urlopen(request)
+        if not f.info().type == "text/xml":
+            rss_url = self.auto_discovery(f)
+            request = urllib2.Request(rss_url)
+            f = urllib2.urlopen(request)
+        print f.read()
+
+    def auto_discovery(self, f):
+        html = f.read()
+        f.close()
+        rss_url_dump = re.search("link.+?type=\"application/rss\+xml\"[^>]+",
+                                 html)
+        rss_url = re.search("(href=\")(http://[^\"]+)",
+                            rss_url_dump.group(0)).group(2)
+        return rss_url
