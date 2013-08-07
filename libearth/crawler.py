52a9839f75177a2b409e9c41d68dd8350e42c33c
Choi Bochul <vio.bo94@gmail.com>
2013-08-07 17:18:11 +0900

    Add class attributes and methods.
    __init__(), get_document_type, reset method. And methods normally return to
    set class attributes.

--- libearth/crawler.py
+++ libearth/crawler.py
@@ -6,15 +6,38 @@ import os
 import re
 import urllib2
 
+from .compat import string_type
+
 
 class Crawler(object):
+    
+    document_rss_url = None
+    document_type = None
+    document_xml = None
 
-    def crawl(self, url):
-        rss_url = self.auto_descovery(url)
-        request = urllib2.Request(url)
+    def __init__(self, url):
+        if not isinstance(url, string_type):
+            raise TypeError(
+                '__tag__ has to be a string, not ' + repr(url)
+            )
+        self.document_rss_url = self.auto_discovery(url)
+        self.document_type = self.get_document_type()
+        self.document_xml = self.crawl()
+
+    def reset(self, url):
+        if not isinstance(url, string_type):
+            raise TypeError(
+                '__tag__ has to be a string, not ' + repr(url)
+            )
+        self.document_rss_url = self.auto_discovery(url)
+        self.document_type = self.get_document_type() 
+        self.document_xml = self.crawl()
+
+    def crawl(self):
+        request = urllib2.Request(self.document_rss_url)
         f = urllib2.urlopen(request)
-        print f.read()
-        f.close()
+        xml = f.read()
+        return xml
 
     def auto_discovery(self, url):
         """If given url is rss feed url, it returns the url instantly.
@@ -23,7 +46,9 @@ class Crawler(object):
         """
         request = urllib2.Request(url)
         f = urllib2.urlopen(request)
-        if f.info().type == 'text/xml':
+        if f.info().type in ('text/xml', 
+                             'application/xml',
+                             'application/atom+xml'):
             f.close()
             return url
         else:
@@ -33,5 +58,13 @@ class Crawler(object):
                 '<link\s.+?type=\"application/rss\+xml\"[^>]+',
                 html)
             rss_url = re.search('(href=\")(http://[^\"]+)',
-                                rss_url_dump.group(0)).group(2)
+                               rss_url_dump.group(0)).group(2)
             return rss_url
+
+    def get_document_type(self):
+        request = urllib2.Request(self.document_rss_url)
+        f = urllib2.urlopen(request)
+        if f.info().type in ('text/xml', 'application/xml'):
+            return 'RSS'
+        elif f.info().type == 'application/atom+xml':
+            return 'Atom'
