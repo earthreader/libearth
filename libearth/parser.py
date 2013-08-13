24a0bde3aa2e3a4b1738d8a5328cf0353d8258c9
Choi Bochul <vio.bo94@gmail.com>
2013-08-13 20:07:23 +0900

    Parsing required tags in Feed Document.

--- libearth/parser.py
+++ libearth/parser.py
@@ -2,7 +2,7 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-
+import datetime
 import re
 import sys
 try:
@@ -22,14 +22,13 @@ class RequiredDuplicatedError(ParserError, AttributeError):
     """Error which rieses when a tag which has to be only one defined
     twice"""
 
-ATOM_XMLNS = 'http://www.w3.org/2005/Atom'
-ATOM_XMLNS_TAG = '{http://www.w3.org/2005/Atom}'
-XMLNS_XML_TAG = "{http://www.w3.org/XML/1998/namespace}"
+XMLNS_ATOM = '{http://www.w3.org/2005/Atom}'
+XMLNS_XML = "{http://www.w3.org/XML/1998/namespace}"
 
 
 def parse_atom(xml):
     root = etree.fromstring(xml)
-    entries = root.findall(ATOM_XMLNS_TAG + 'entry')
+    entries = root.findall(XMLNS_ATOM + 'entry')
     feed_data = atom_get_feed_data(root)
     entry_data = atom_get_entry_data(entries)
     return feed_data
@@ -43,13 +42,19 @@ def atom_get_feed_data(root):
         feed_data[tag] = []
     _iter = root.iter() if sys.version_info >= (2, 7) else root.getiterator()
     data = next(_iter)
-    while not data.tag == ATOM_XMLNS_TAG + 'entry':
-        if data.tag == ATOM_XMLNS_TAG + 'id':
-            if 'id' in feed_data:
-                raise RequiredDuplicatedError(
-                    'required tag duplicated')
+    while not data.tag == XMLNS_ATOM + 'entry':
+        if data.tag == XMLNS_ATOM + 'id':
             feed_data['id'] = atom_common_attribute(data, None)
             feed_data['id']['uri'] = data.text
+        elif data.tag == XMLNS_ATOM + 'title':
+            title_optional_attrib = [XMLNS_ATOM + 'type']
+            feed_data['title'] = atom_common_attribute(data,
+                                                       title_optional_attrib)
+            feed_data['title']['type'] = data.attrib['type']
+            feed_data['title']['text'] = data.text
+        elif data.tag == XMLNS_ATOM + 'updated':
+            feed_data['updated'] = atom_common_attribute(data, None)
+            feed_data['updated']['datetime'] = atom_date_time(data.text)
         data = next(_iter)
     return feed_data
 
@@ -61,10 +66,44 @@ def atom_get_entry_data(entried):
 def atom_common_attribute(data, *args):
     common_attribute = {}
     for attrib in data.attrib:
-        if attrib == XMLNS_XML_TAG + 'base':
+        if attrib == XMLNS_XML + 'base':
             common_attribute['xml:base'] = data.attrib[attrib]
-        elif attrib == XMLNS_XML_TAG + 'lang':
+        elif attrib == XMLNS_XML + 'lang':
             common_attribute['xml:lang'] = data.attrib[attrib]
         elif not attrib in args:
-            kcommon_attribute[attrib] = data.attrib[attrib]
+            common_attribute[attrib] = data.attrib[attrib]
     return common_attribute
+
+
+class FixedOffset(datetime.tzinfo):
+
+    def __init__(self, offset):
+        self.sign = offset[0]
+        self.hours = offset[1:3]
+        self.minutes = offset[4:6]
+
+    def utcoffset(self, dt):
+        if self.sign == '+':
+            return datetime.timedelta(hours=int(self.hours),
+                                      minutes=int(self.minutes))
+        else:
+            return datetime.timedelta(hours=int(self.hours)*(-1),
+                                      minutes=int(self.minutes)*(-1))
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+
+def atom_date_time(text):
+    date_and_time = text[:19]
+    second_fraction_and_timezone = re.search('\.?([^\+\-Z]*)(.+)', text[19:])
+    datetime_object = datetime.datetime.strptime(date_and_time,
+                                                 '%Y-%m-%dT%H:%M:%S')
+    if second_fraction_and_timezone.group(1):
+        second_fraction = second_fraction_and_timezone.group(1)
+        microsecond = int(second_fraction)*pow(10, 6-len(second_fraction))
+        datetime_object = datetime_object.replace(microsecond=microsecond)
+    if not second_fraction_and_timezone.group(2).startswith('Z'):
+        offset = second_fraction_and_timezone.group(2)
+        datetime_object = datetime_object.replace(tzinfo=FixedOffset(offset))
+    return datetime_object
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -2,7 +2,9 @@ from libearth.parser import parse_atom
 
 atom_xml = """
 <feed xmlns="http://www.w3.org/2005/Atom" xmlns:test="http://test.xmlns.com">
+    <title type="text">Atom Test</title>
     <id>http://vio.atomtest.com/feed/atom/</id>
+    <updated>2013-08-19T07:49:20+07:00</updated>
     <link rel="alternate" type="text/html" href="http://vio.atomtest.com/" />
     <link rel="self" type="application/atom+xml"
         href="http://vio.atomtest.com/feed/atom/" />
@@ -25,3 +27,6 @@ atom_xml = """
 def test_atom_parser():
     feed_data = parse_atom(atom_xml)
     assert feed_data['id']['uri'] == 'http://vio.atomtest.com/feed/atom/'
+    assert feed_data['title']['text'] == 'Atom Test'
+    assert feed_data['updated']['datetime'].isoformat() \
+        == '2013-08-19T07:49:20+07:00'
