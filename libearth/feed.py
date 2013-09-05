6b447ba5afbd0cdd2fb3675bf34ef4ac1aba887f
Hong Minhee <minhee@dahlia.kr>
2013-09-06 03:32:23 +0900

    Person construct

--- libearth/feed.py
+++ libearth/feed.py
@@ -15,10 +15,14 @@ except ImportError:
     from html import parser as HTMLParser
 
 from .codecs import Enum
-from .compat import UNICODE_BY_DEFAULT
-from .schema import Attribute, Content, Element
+from .compat import UNICODE_BY_DEFAULT, text_type
+from .schema import Attribute, Content, Element, Text as TextChild
 
-__all__ = 'MarkupTagCleaner', 'Text'
+__all__ = 'ATOM_XMLNS', 'MarkupTagCleaner', 'Person', 'Text'
+
+
+#: (:class:`str`) The XML namespace name used for Atom (:rfc:`4287`).
+ATOM_XMLNS = 'http://www.w3.org/2005/Atom'
 
 
 class MarkupTagCleaner(HTMLParser.HTMLParser):
@@ -76,9 +80,53 @@ class Text(Element):
         if self.type == 'html':
             return self.value
         elif self.type == 'text':
-            return cgi.escape(self.value).replace('\n', '<br>\n')
+            return cgi.escape(self.value, quote=True).replace('\n', '<br>\n')
 
     def __repr__(self):
         return '{0.__module__}.{0.__name__}(type={1!r}, value={2!r})'.format(
             type(self), self.type, self.value
         )
+
+
+class Person(Element):
+    """Person construct defined in :rfc:`4287#section-3.2` (section 3.2)."""
+
+    #: (:class:`str`) The human-readable name for the person.  It corresponds
+    #: to ``atom:name`` element of :rfc:`4287#section-3.2.1` (section 3.2.1).
+    name = TextChild('name', xmlns=ATOM_XMLNS, required=True)
+
+    #: (:class:`str`) The optional URI associated with the person.
+    #: It corresponds to ``atom:uri`` element of :rfc:`4287#section-3.2.2`
+    #: (section 3.2.2).
+    uri = TextChild('uri', xmlns=ATOM_XMLNS)
+
+    #: (:class:`str`) The optional email address associated with the person.
+    #: It corresponds to ``atom:email`` element of :rfc:`4287#section-3.2.3`
+    #: (section 3.2.3).
+    email = TextChild('email', xmlns=ATOM_XMLNS)
+
+    def __unicode__(self):
+        ref = self.uri or self.email
+        if ref:
+            return text_type('{0} <{1}>').format(self.name, ref)
+        return self.name
+
+    if UNICODE_BY_DEFAULT:
+        __str__ = __unicode__
+    else:
+        __str__ = lambda self: unicode(self).encode('utf-8')
+
+    def __html__(self):
+        name = cgi.escape(self.name, quote=True)
+        ref = self.uri or self.email and 'mailto:' + self.email
+        if ref:
+            return text_type('<a href="{1}">{0}</a>').format(
+                name,
+                cgi.escape(ref, quote=True)
+            )
+        return name
+
+    def __repr__(self):
+        return ('{0.__module__}.{0.__name__}(name={1!r}, uri={2!r}'
+                ', email={3!r})').format(type(self), self.name, self.uri,
+                                         self.email)
--- tests/feed_test.py
+++ tests/feed_test.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 from libearth.compat import text_type
-from libearth.feed import MarkupTagCleaner, Text
+from libearth.feed import MarkupTagCleaner, Person, Text
+
+
+def u(text):
+    if isinstance(text, text_type):
+        return text
+    return text.decode('utf-8')
 
 
 def test_markup_tag_cleaner():
@@ -26,3 +33,34 @@ def test_text_html():
     assert Text(type='html', value='Hello world').__html__() == 'Hello world'
     assert (Text(type='html', value='<p>Hello <em>world</em></p>').__html__()
             == '<p>Hello <em>world</em></p>')
+
+
+def test_person_str():
+    assert text_type(Person(name='Hong Minhee')) == 'Hong Minhee'
+    assert (text_type(Person(name='Hong Minhee', uri='http://dahlia.kr/')) ==
+            'Hong Minhee <http://dahlia.kr/>')
+    email = '\x6d\x69\x6e\x68\x65\x65\x40\x64\x61\x68\x6c\x69\x61\x2e\x6b\x72'
+    assert (text_type(Person(name='Hong Minhee', email=email)) ==
+            'Hong Minhee <' + email + '>')
+    assert u('홍민희 <http://dahlia.kr/>') == text_type(
+        Person(
+            name=u('홍민희'),
+            uri='http://dahlia.kr/',
+            email=email
+        )
+    )
+
+
+def test_person_html():
+    assert (Person(name='Hong "Test" Minhee').__html__() ==
+            'Hong &quot;Test&quot; Minhee')
+    assert (Person(name='Hong Minhee', uri='http://dahlia.kr/').__html__() ==
+            '<a href="http://dahlia.kr/">Hong Minhee</a>')
+    email = '\x6d\x69\x6e\x68\x65\x65\x40\x64\x61\x68\x6c\x69\x61\x2e\x6b\x72'
+    assert (Person(name='Hong Minhee', email=email).__html__() ==
+            '<a href="mailto:' + email + '">Hong Minhee</a>')
+    assert u('<a href="http://dahlia.kr/">홍민희</a>') == Person(
+        name=u('홍민희'),
+        uri='http://dahlia.kr/',
+        email=email
+    ).__html__()
