e8453e9a03c9224ce8fc7a23338568cda41ba3ab
Hong Minhee <minhee@dahlia.kr>
2013-09-06 03:03:03 +0900

    Text construct

--- libearth/feed.py
+++ libearth/feed.py
@@ -8,12 +8,17 @@ follow robustness principle or such thing.  It simply treats stored data are
 all valid and well-formed.
 
 """
+import cgi
 try:
     import HTMLParser
 except ImportError:
     from html import parser as HTMLParser
 
-__all__ = 'MarkupTagCleaner',
+from .codecs import Enum
+from .compat import UNICODE_BY_DEFAULT
+from .schema import Attribute, Content, Element
+
+__all__ = 'MarkupTagCleaner', 'Text'
 
 
 class MarkupTagCleaner(HTMLParser.HTMLParser):
@@ -31,3 +36,49 @@ class MarkupTagCleaner(HTMLParser.HTMLParser):
 
     def handle_data(self, d):
         self.fed.append(d)
+
+
+class Text(Element):
+    """Text construct defined in :rfc:`4287#section-3.1` (section 3.1)."""
+
+    #: (:class:`str`) The type of the text.  It could be one of ``'text'``
+    #: or ``'html'``.  It corresponds to :rfc:`4287#section-3.1.1` (section
+    #: 3.1.1).
+    #:
+    #: .. note::
+    #:
+    #:    It currently does not support ``'xhtml'``.
+    #:
+    #: .. todo::
+    #:
+    #:    Default value should be ``'text'``.
+    type = Attribute('type', Enum(['text', 'html']), required=True)
+
+    #: (:class:`str`) The content of the text.  Interpretation for this
+    #: has to differ according to its :attr:`type`.  It corresponds to
+    #: :rfc:`4287#section-3.1.1.1` (section 3.1.1.1) if :attr:`type` is
+    #: ``'text'``, and :rfc:`4287#section-3.1.1.2` (section 3.1.1.2) if
+    #: :attr:`type` is ``'html'``.
+    value = Content()
+
+    def __unicode__(self):
+        if self.type == 'html':
+            return MarkupTagCleaner.clean(self.value)
+        elif self.type == 'text':
+            return self.value
+
+    if UNICODE_BY_DEFAULT:
+        __str__ = __unicode__
+    else:
+        __str__ = lambda self: unicode(self).encode('utf-8')
+
+    def __html__(self):
+        if self.type == 'html':
+            return self.value
+        elif self.type == 'text':
+            return cgi.escape(self.value).replace('\n', '<br>\n')
+
+    def __repr__(self):
+        return '{0.__module__}.{0.__name__}(type={1!r}, value={2!r})'.format(
+            type(self), self.type, self.value
+        )
--- tests/feed_test.py
+++ tests/feed_test.py
@@ -1,7 +1,28 @@
-from libearth.feed import MarkupTagCleaner
+from libearth.compat import text_type
+from libearth.feed import MarkupTagCleaner, Text
 
 
 def test_markup_tag_cleaner():
     assert MarkupTagCleaner.clean('<b>Hello</b>') == 'Hello'
     assert MarkupTagCleaner.clean('<p><b>Hello</b></p>') == 'Hello'
     assert MarkupTagCleaner.clean('<p>Hello <b>world</b></p>') == 'Hello world'
+
+
+def test_text_str():
+    assert text_type(Text(type='text', value='Hello world')) == 'Hello world'
+    assert (text_type(Text(type='text', value='<p>Hello <em>world</em></p>'))
+            == '<p>Hello <em>world</em></p>')
+    assert text_type(Text(type='html', value='Hello world')) == 'Hello world'
+    assert (text_type(Text(type='html', value='<p>Hello <em>world</em></p>'))
+            == 'Hello world')
+
+
+def test_text_html():
+    assert Text(type='text', value='Hello world').__html__() == 'Hello world'
+    assert (Text(type='text', value='Hello\nworld').__html__() ==
+            'Hello<br>\nworld')
+    assert (Text(type='text', value='<p>Hello <em>world</em></p>').__html__()
+            == '&lt;p&gt;Hello &lt;em&gt;world&lt;/em&gt;&lt;/p&gt;')
+    assert Text(type='html', value='Hello world').__html__() == 'Hello world'
+    assert (Text(type='html', value='<p>Hello <em>world</em></p>').__html__()
+            == '<p>Hello <em>world</em></p>')
