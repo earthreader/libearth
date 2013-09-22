0a3dc8fe2065c8041acc4cd1ec7524b39948393e
Hong Minhee <minhee@dahlia.kr>
2013-09-22 13:04:54 +0900

    Codec to store session+revision time

--- libearth/session.py
+++ libearth/session.py
@@ -1,43 +1,93 @@
-""":mod:`libearth.session` --- Sessions
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The basic concept of Earth Reader eventually arrives at a problem: how can
-we merge simultaneous updates of the same feed without any conflicts?
-It's a sort of well known problems in computer science, and there are several
-solutions for it e.g. MVCC_ (multiversion concurrency control).
-
-::
-
-    repository = Repository('/path/to/save/files')
-    session = Session(repository)
-
-.. _MVCC: http://en.wikipedia.org/wiki/Multiversion_concurrency_control
+""":mod:`libearth.session` --- Merging concurrent updates between devices
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import collections
+import datetime
+import re
 import uuid
 
-from .repository import Repository
+from .codecs import Rfc3339
+from .compat import string_type
+from .schema import Codec
 
-__all__ = 'Session',
+__all__ = 'SESSION_XMLNS', 'Revision', 'RevisionCodec', 'Session'
+
+
+#: (:class:`str`) The XML namespace name used for session metadata.
+SESSION_XMLNS = 'http://earthreader.github.io/session/'
 
 
 class Session(object):
 
-    #: (:class:`~libearth.repository.Repository`) The repository to read and
-    #: write.
-    repository = None
+    #: (:class:`re.RegexObject`) The regular expression pattern that matches
+    #: to allowed identifiers.
+    IDENTIFIER_PATTERN = re.compile(r'^[-a-z0-9_.]+$', re.IGNORECASE)
+
+    #: (:class:`collections.MutableMapping`) The pool of interned sessions.
+    #: It's for maintaining single sessions for the same identifiers.
+    interns = {}
 
     #: (:class:`str`) The session identifier.  It has to be distinguishable
     #: from other devices/apps, but consistent for the same device/app.
     identifier = None
 
-    def __init__(self, repository, identifier=None):
-        if not isinstance(repository, Repository):
-            raise TypeError('repository must be a {0.__module__}.{0.__name__}'
-                            ', not {1!r}'.format(Repository, repository))
-        self.repository = repository
-        self.identifier = identifier or str(uuid.getnode())
+    def __new__(cls, identifier=None):
+        if not (identifier is None or isinstance(identifier, string_type)):
+            raise TypeError('identifier must be a string, not ' +
+                            repr(identifier))
+        identifier = str(identifier) if identifier else str(uuid.getnode())
+        if not cls.IDENTIFIER_PATTERN.match(identifier):
+            raise ValueError('invalid identifier format: ' + repr(identifier))
+        try:
+            session = cls.interns[identifier]
+        except KeyError:
+            session = super(Session, cls).__new__(cls)
+            session.identifier = identifier
+            cls.interns[identifier] = session
+        return session
+
+    def __str__(self):
+        return self.identifier
 
     def __repr__(self):
         return '{0.__module__}.{0.__name__}({1!r})'.format(type(self),
-                                                           self.path)
+                                                           self.identifier)
+
+
+#: The named tuple type of (:class:`Session`, :class:`datetime.datetime`) pair.
+Revision = collections.namedtuple('Revision', 'session updated_at')
+
+
+class RevisionCodec(Codec):
+    """Codec to encode/decode :class:`Revision` pairs.
+
+    >>> from libearth.tz import utc
+    >>> session = Session('test-identifier')
+    >>> updated_at = datetime.datetime(2013, 9, 22, 3, 43, 40, tzinfo=utc)
+    >>> rev = Revision(session, updated_at)
+    >>> RevisionCodec().encode(rev)
+    'test-identifier 2013-09-22T03:43:40Z'
+
+    """
+
+    #: (:class:`Rfc3339`) The intenally used codec to encode
+    #: :attr:`Revision.updated_at` time to :rfc:`3339` format.
+    RFC3339_CODEC = Rfc3339(prefer_utc=True)
+
+    def encode(self, value):
+        session, updated_at = value
+        if not isinstance(session, Session):
+            raise TypeError('{0!r} is not an instance of {1.__module__}.'
+                            '{1.__name__}'.format(session, Session))
+        elif not isinstance(updated_at, datetime.datetime):
+            raise TypeError('{0!r} is not an instance of {1.__module__}.{1.'
+                            '__name__}'.format(updated_at, datetime.datetime))
+        return '{0} {1}'.format(session, self.RFC3339_CODEC.encode(updated_at))
+
+    def decode(self, text):
+        identifier, updated_at = text.split()
+        return Revision(
+            session=Session(identifier),
+            updated_at=self.RFC3339_CODEC.decode(updated_at)
+        )
--- tests/session_test.py
+++ tests/session_test.py
@@ -1,10 +1,64 @@
+import datetime
+
 from pytest import raises
 
-from libearth.session import Session
+from libearth.session import Revision, RevisionCodec, Session
+from libearth.tz import now, utc
 
 
-def test_repository_type_error():
-    with raises(TypeError):
-        Session(1234)
-    with raises(TypeError):
-        Session('abc')
+def test_intern():
+    """Session of the same identifier cannot be multiple."""
+    session = Session('id1')
+    assert session is Session('id1')
+    assert session is not Session('id2')
+
+
+def test_default_identifier():
+    assert Session().identifier != ''
+    assert Session().identifier is not None
+    assert Session().identifier != Session.identifier
+
+
+def test_invalid_identifier():
+    with raises(ValueError):
+        Session('i n v a l i d')
+    with raises(ValueError):
+        Session('i*n*v*a*l*i*d')
+    with raises(ValueError):
+        Session('i+n+v+a+l+i+d')
+    with raises(ValueError):
+        Session('i/n/v/a/l/i/d')
+    with raises(ValueError):
+        Session('i\nn\nv\na\nl\ni\nd')
+    with raises(ValueError):
+        Session('i\tn\tv\ta\tl\ti\td')
+    with raises(ValueError):
+        Session('(invalid)')
+    Session('valid')
+    Session('v-a-l-i-d')
+    Session('v.a.l.i.d')
+    Session('v_a_l_i_d')
+    Session('v1a2l3i4d')
+    Session('v-a.l_i4d')
+
+
+def test_revision():
+    session = Session()
+    updated_at = now()
+    rev = Revision(session, updated_at)
+    assert rev == (session, updated_at)
+    assert rev[0] is rev.session is session
+    assert rev[1] == rev.updated_at == updated_at
+
+
+def test_revision_codec():
+    session = Session('test-identifier')
+    updated_at = datetime.datetime(2013, 9, 22, 3, 43, 40, tzinfo=utc)
+    rev = Revision(session, updated_at)
+    codec = RevisionCodec()
+    assert codec.encode(rev) == 'test-identifier 2013-09-22T03:43:40Z'
+    assert codec.encode(tuple(rev)) == 'test-identifier 2013-09-22T03:43:40Z'
+    decoded = codec.decode('test-identifier 2013-09-22T03:43:40Z')
+    assert decoded == rev
+    assert decoded.session is session
+    assert decoded.updated_at == updated_at
