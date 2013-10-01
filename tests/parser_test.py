ec3c6acf2f7ff040ef5915040f048b20c4b39e42
Choi Bochul <vio.bo94@gmail.com>
2013-10-01 15:04:11 +0900

    Test parser with read function to avoid attributes mistyping

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -3,7 +3,9 @@ import datetime
 import httpretty
 
 from libearth.codecs import Rfc3339
+from libearth.feed import Feed
 from libearth.parser import atom, rss2, autodiscovery
+from libearth.schema import read, write
 from libearth.tz import utc
 
 
@@ -92,60 +94,62 @@ atom_xml = """
 
 def test_atom_parser():
     url = 'http://vio.atomtest.com/feed/atom'
-    feed_data, _ = atom.parse_atom(atom_xml, url)
-    title = feed_data.title
-    assert title.type == 'text'
-    assert title.value == 'Atom Test'
-    subtitle = feed_data.subtitle
-    assert subtitle.type == 'text'
-    assert subtitle.value == 'Earth Reader'
-    links = feed_data.links
-    assert links[0].relation == 'alternate'
-    assert links[0].mimetype == 'text/html'
-    assert links[0].uri == 'http://vio.atomtest.com/'
-    assert links[1].relation == 'self'
-    assert links[1].mimetype == 'application/atom+xml'
-    assert links[1].uri == 'http://vio.atomtest.com/feed/atom'
-    authors = feed_data.authors
-    assert authors[0].name == 'vio'
-    assert authors[0].email == 'vio.bo94@gmail.com'
-    categories = feed_data.categories
-    assert categories[0].term == 'Python'
-    contributors = feed_data.contributors
-    assert contributors[0].name == 'dahlia'
-    generator = feed_data.generator
-    assert generator.uri == 'http://wordpress.com/'
-    assert generator.value == 'WordPress.com'
-    icon = feed_data.icon
-    assert icon == 'http://vio.atomtest.com/images/icon.jpg'
-    logo = feed_data.logo
-    assert logo == 'http://vio.atomtest.com/images/logo.jpg'
-    rights = feed_data.rights
-    assert rights.type == 'text'
-    assert rights.value == 'vio company all rights reserved'
-    updated_at = feed_data.updated_at
-    assert updated_at == Rfc3339().decode('2013-08-10T15:27:04Z')
-    entries = feed_data.entries
-    assert entries[0].id == 'http://vio.atomtest.com/feed/one'
-    assert entries[0].authors[0].name == 'vio'
-    assert entries[0].title.type == 'text'
-    assert entries[0].title.value == 'Title One'
-    assert entries[0].links[0].relation == 'self'
-    assert entries[0].links[0].uri == 'http://vio.atomtest.com/?p=12345'
-    assert entries[0].updated_at == Rfc3339().decode('2013-08-10T15:27:04Z')
-    assert entries[0].published_at == datetime.datetime(2013, 8, 10, 15, 26, 15,
-                                                        tzinfo=utc)
-    assert entries[0].categories[0].scheme == 'http://vio.atomtest.com'
-    assert entries[0].categories[0].term == 'Category One'
-    assert entries[0].categories[1].scheme == 'http://vio.atomtest.com'
-    assert entries[0].categories[1].term == 'Category Two'
-    assert entries[0].content.type == 'text'
-    assert entries[0].content.value == 'Hello World'
-    assert entries[1].id == 'http://basetest.com/two'
-    assert entries[1].authors[0].name == 'kjwon'
-    assert entries[1].title.type == 'text'
-    assert entries[1].title.value == 'xml base test'
-    assert entries[1].updated_at == Rfc3339().decode('2013-08-17T03:28:11Z')
+    crawled_feed, _ = atom.parse_atom(atom_xml, url)
+    feed = read(Feed, write(crawled_feed))
+    title = crawled_feed.title
+    assert title.type == feed.title.type
+    assert title.value == feed.title.value
+    subtitle = crawled_feed.subtitle
+    assert subtitle.type == feed.subtitle.type
+    assert subtitle.value == feed.subtitle.value
+    links = crawled_feed.links
+    assert links[0].relation == feed.links[0].relation
+    assert links[0].mimetype == feed.links[0].mimetype
+    assert links[0].uri == feed.links[0].uri
+    assert links[1].relation == feed.links[1].relation
+    assert links[1].mimetype == feed.links[1].mimetype
+    assert links[1].uri == feed.links[1].uri
+    authors = crawled_feed.authors
+    assert authors[0].name == feed.authors[0].name
+    assert authors[0].email == feed.authors[0].email
+    categories = crawled_feed.categories
+    assert categories[0].term == feed.categories[0].term
+    contributors = crawled_feed.contributors
+    assert contributors[0].name == feed.contributors[0].name
+    generator = crawled_feed.generator
+    assert generator.uri == feed.generator.uri
+    assert generator.value == feed.generator.value
+    icon = crawled_feed.icon
+    assert icon == feed.icon
+    logo = crawled_feed.logo
+    assert logo == feed.logo
+    rights = crawled_feed.rights
+    assert rights.type == feed.rights.type
+    assert rights.value == feed.rights.value
+    updated_at = crawled_feed.updated_at
+    assert updated_at == feed.updated_at
+    entries = crawled_feed.entries
+    assert entries[0].id == feed.entries[0].id
+    assert entries[0].authors[0].name == feed.entries[0].authors[0].name
+    assert entries[0].title.type == feed.entries[0].title.type
+    assert entries[0].title.value == feed.entries[0].title.value
+    assert entries[0].links[0].relation == feed.entries[0].links[0].relation
+    assert entries[0].links[0].uri == feed.entries[0].links[0].uri
+    assert entries[0].updated_at == feed.entries[0].updated_at
+    assert entries[0].published_at == feed.entries[0].published_at
+    assert entries[0].categories[0].scheme_uri == \
+        feed.entries[0].categories[0].scheme_uri
+    assert entries[0].categories[0].term == feed.entries[0].categories[0].term
+    assert entries[0].categories[1].scheme_uri == \
+        feed.entries[0].categories[1].scheme_uri
+    assert entries[0].categories[1].term == feed.entries[0].categories[1].term
+    assert entries[0].content.type == feed.entries[0].content.type
+    assert entries[0].content.value == feed.entries[0].content.value
+    assert entries[1].id == feed.entries[1].id
+    assert entries[1].authors[0].name == feed.entries[1].authors[0].name
+    assert entries[1].title.type == feed.entries[1].title.type
+    assert entries[1].title.value == feed.entries[1].title.value
+    assert entries[1].updated_at == feed.entries[1].updated_at
 
 
 rss_xml = """
@@ -183,6 +187,7 @@ rss_source_xml = """
         <title>Source Test</title>
         <link>http://sourcetest.com/</link>
         <description>for source tag test</description>
+        <pubDate>Sat, 17 Sep 2002 00:00:01 GMT</pubDate>
         <item>
             <title>It will not be parsed</title>
         </item>
@@ -195,53 +200,57 @@ rss_source_xml = """
 def test_rss_parser():
     httpretty.register_uri(httpretty.GET, "http://sourcetest.com/rss.xml",
                            body=rss_source_xml)
-    feed_data, data_for_crawl = rss2.parse_rss(rss_xml)
-    title = feed_data.title
-    assert title.type == 'text'
-    assert title.value == 'Vio Blog'
-    links = feed_data.links
-    assert links[1].type == 'text/html'
-    assert links[1].relation == 'alternate'
-    assert links[1].uri == 'http://vioblog.com'
-    rights = feed_data.rights
-    assert rights.type == 'text'
-    assert rights.value == 'Copyright2013, Vio'
-    contributors = feed_data.contributors
-    assert contributors[0].name == 'vio.bo94@gmail.com'
-    assert contributors[0].email == 'vio.bo94@gmail.com'
-    assert contributors[1].name == 'vio.bo94@gmail.com'
-    assert contributors[1].email == 'vio.bo94@gmail.com'
-    updated_at = feed_data.updated_at
-    assert updated_at == datetime.datetime(2002, 9, 17, 0, 0, 1, tzinfo=utc)
-    categories = feed_data.categories
-    assert categories[0].term == 'Python'
-    entries = feed_data.entries
-    assert entries[0].title.type == 'text'
-    assert entries[0].title.value == 'test one'
-    assert entries[0].links[0].type == 'text/html'
-    assert entries[0].links[0].relation == 'alternate'
-    assert entries[0].links[0].uri == 'http://vioblog.com/12'
-    assert entries[0].content.value == 'This is the content'
-    assert entries[0].authors[0].name == 'vio.bo94@gmail.com'
-    assert entries[0].authors[0].email == 'vio.bo94@gmail.com'
-    assert entries[0].links[1].type == 'audio/mpeg'
-    assert entries[0].links[1].uri == 'http://vioblog.com/mp/a.mp3'
-    assert entries[0].id == 'http://vioblog.com/12'
+    crawled_feed, data_for_crawl = rss2.parse_rss(rss_xml,
+        'http://sourcetest.com/rss.xml')
+    feed = read(Feed, write(crawled_feed))
+    assert crawled_feed.id == feed.id
+    title = crawled_feed.title
+    assert title.type == feed.title.type
+    assert title.value == feed.title.value
+    links = crawled_feed.links
+    assert links[1].mimetype == feed.links[1].mimetype
+    assert links[1].relation == feed.links[1].relation
+    assert links[1].uri == feed.links[1].uri
+    rights = crawled_feed.rights
+    assert rights.type == feed.rights.type
+    assert rights.value == feed.rights.value
+    contributors = crawled_feed.contributors
+    assert contributors[0].name == feed.contributors[0].name
+    assert contributors[0].email == feed.contributors[0].email
+    assert contributors[1].name == feed.contributors[1].name
+    assert contributors[1].email == feed.contributors[1].email
+    updated_at = crawled_feed.updated_at
+    assert updated_at == feed.updated_at
+    categories = crawled_feed.categories
+    assert categories[0].term == feed.categories[0].term
+    entries = crawled_feed.entries
+    assert entries[0].title.type == feed.entries[0].title.type
+    assert entries[0].title.value == feed.entries[0].title.value
+    assert entries[0].links[0].mimetype == feed.entries[0].links[0].mimetype
+    assert entries[0].links[0].relation == feed.entries[0].links[0].relation
+    assert entries[0].links[0].uri == feed.entries[0].links[0].uri
+    assert entries[0].content.value == feed.entries[0].content.value
+    assert entries[0].authors[0].name == feed.entries[0].authors[0].name
+    assert entries[0].authors[0].email == feed.entries[0].authors[0].email
+    assert entries[0].links[1].mimetype == feed.entries[0].links[1].mimetype
+    assert entries[0].links[1].uri == feed.entries[0].links[1].uri
+    assert entries[0].id == feed.entries[0].id
     assert (entries[0].published_at ==
             entries[0].updated_at ==
-            datetime.datetime(2002, 9, 7, 0, 0, 1, tzinfo=utc))
+            feed.entries[0].published_at ==
+            feed.entries[0].updated_at)
     assert data_for_crawl == {
         'lastBuildDate': datetime.datetime(2002, 9, 7, 0, 0, 1, tzinfo=utc),
         'ttl': '10',
     }
     source = entries[0].source
-    assert source.title.type == 'text'
-    assert source.title.value == 'Source Test'
-    assert source.links[1].type == 'text/html'
-    assert source.links[1].uri == 'http://sourcetest.com/'
-    assert source.links[1].relation == 'alternate'
-    assert source.subtitle.type == 'text'
-    assert source.subtitle.value == 'for source tag test'
+    assert source.title.type == feed.entries[0].source.title.type
+    assert source.title.value == feed.entries[0].source.title.value
+    assert source.links[1].mimetype == feed.entries[0].source.links[1].mimetype
+    assert source.links[1].uri == feed.entries[0].source.links[1].uri
+    assert source.links[1].relation == feed.entries[0].source.links[1].relation
+    assert source.subtitle.type == feed.entries[0].source.subtitle.type
+    assert source.subtitle.value == feed.entries[0].source.subtitle.value
     assert not source.entries
 
 
@@ -256,5 +265,5 @@ category_with_no_term = '''
 
 
 def test_category_with_no_term():
-    feed_data, crawler_hints = atom.parse_atom(category_with_no_term, None)
-    assert not feed_data.categories
+    crawled_feed, crawler_hints = atom.parse_atom(category_with_no_term, None)
+    assert not crawled_feed.categories
