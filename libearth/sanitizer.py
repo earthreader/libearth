5580ec33477e36e42d31e3aeaa537d8ce94f1374
Hong Minhee <minhee@dahlia.kr>
2013-10-12 01:23:04 +0900

    Fix bug that clean_html() ignores entity chars

--- libearth/sanitizer.py
+++ libearth/sanitizer.py
@@ -3,9 +3,12 @@
 
 """
 try:
+    import htmlentitydefs
     import HTMLParser
 except ImportError:
-    from html import parser as HTMLParser
+    from html import entities as htmlentitydefs, parser as HTMLParser
+
+from .compat import unichr
 
 __all__ = 'MarkupTagCleaner', 'clean_html'
 
@@ -23,9 +26,26 @@ def clean_html(html):
 class MarkupTagCleaner(HTMLParser.HTMLParser):
     """HTML parser that is internally used by :func:`clean_html()` function."""
 
+    entity_map = htmlentitydefs.name2codepoint
+
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
         self.fed = []
 
     def handle_data(self, d):
         self.fed.append(d)
+
+    def handle_entityref(self, name):
+        try:
+            codepoint = self.entity_map[name]
+        except KeyError:
+            pass
+        else:
+            self.fed.append(unichr(codepoint))
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            codepoint = int(name[1:], 16)
+        else:
+            codepoint = int(name)
+        self.fed.append(unichr(codepoint))
--- tests/sanitizer_test.py
+++ tests/sanitizer_test.py
@@ -5,3 +5,6 @@ def test_markup_tag_cleaner():
     assert clean_html('<b>Hello</b>') == 'Hello'
     assert clean_html('<p><b>Hello</b></p>') == 'Hello'
     assert clean_html('<p>Hello <b>world</b></p>') == 'Hello world'
+    assert (clean_html('<p>&quot;&lt;Entity&gt; test&quot;</p>') ==
+            '"<Entity> test"')
+    assert clean_html('<p>&#34;charref test&#x22;</p>') == '"charref test"'
