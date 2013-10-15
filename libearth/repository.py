ad09c1a660704a61140d35fe3f5480c29bdc3a79
Hong Minhee <minhee@dahlia.kr>
2013-10-15 11:37:35 +0900

    Basic operations on repository

--- libearth/repository.py
+++ libearth/repository.py
@@ -2,12 +2,105 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import collections
+import os
 import os.path
 
-__all__ = 'FileNotFoundError', 'NotADirectoryError', 'Repository'
+from .compat import xrange
+
+__all__ = ('FileNotFoundError', 'FileSystemRepository', 'NotADirectoryError',
+           'Repository', 'RepositoryKeyError')
 
 
 class Repository(object):
+    """Repository interface agnostic to its underlying storage implementation.
+    :class:`~libearth.stage.Stage` objects can deal with documents to be stored
+    using the interface.
+
+    """
+
+    def read(self, key):
+        """Read the content from the ``key``.
+
+        :param key: the key which stores the content to read
+        :type key: :class:`collections.Sequence`
+        :returns: byte string chunks
+        :rtype: :class:`collections.Iterable`
+        :raises RepositoryKeyError: the ``key`` cannot be found in
+                                    the repository
+
+        .. note::
+
+           Every subclass of :class:`Repository` has to override
+           :meth:`read()` method to implement details.
+
+        """
+        if not isinstance(key, collections.Sequence):
+            raise TypeError('key must be a sequence, not ' + repr(key))
+        elif not key:
+            raise RepositoryKeyError(key, 'key cannot be empty')
+        if hash(type(self).read) == hash(Repository.read):
+            raise NotImplementedError(
+                'every subclass of {0.__module__}.{0.__name__} has to '
+                'implement read() method'.format(Repository)
+            )
+
+    def write(self, key, iterable):
+        """Write the ``iterable`` into the ``key``.
+
+        :param key: the key to stores the ``iterable``
+        :type key: :class:`collections.Sequence`
+        :param iterable: the iterable object yiels chunks of the whole
+                         content.  every chunk has to be a byte string
+        :type iterable: :class:`collections.Iterable`
+
+        .. note::
+
+           Every subclass of :class:`Repository` has to override
+           :meth:`write()` method to implement details.
+
+        """
+        if not isinstance(key, collections.Sequence):
+            raise TypeError('key must be a sequence, not ' + repr(key))
+        elif not isinstance(iterable, collections.Iterable):
+            raise TypeError('expected an iterable object, not ' +
+                            repr(iterable))
+        elif not key:
+            raise RepositoryKeyError(key, 'key cannot be empty')
+        if hash(type(self).write) == hash(Repository.write):
+            raise NotImplementedError(
+                'every subclass of {0.__module__}.{0.__name__} has to '
+                'implement write() method'.format(Repository)
+            )
+
+    def __repr__(self):
+        return '{0.__module__}.{0.__name__}()'.format(type(self))
+
+
+class RepositoryKeyError(LookupError, IOError):
+    """Exception which rises when the requested key cannot be found
+    in the repository.
+
+    """
+
+    #: (:class:`collections.Sequence`) The requested key.
+    key = None
+
+    def __init__(self, key, *args, **kwargs):
+        super(RepositoryKeyError, self).__init__(*args, **kwargs)
+        self.key = key
+
+
+class FileSystemRepository(Repository):
+    """Builtin implementation of :class:`Repository` interface which uses
+    the ordinary file system.
+
+    :param path: the directory path to store keys
+    :type path: :class:`str`
+    :raises FileNotFoundError: when the ``path`` doesn't exist
+    :raises NotADirectoryError: when the ``path`` is not a directory
+
+    """
 
     #: (:class:`str`) The path of the directory to read and write data files.
     #: It should be readable and writable.
@@ -19,6 +112,35 @@ class Repository(object):
         elif not os.path.isdir(path):
             raise NotADirectoryError(repr(path) + ' is not a directory')
         self.path = path
+
+    def read(self, key):
+        super(FileSystemRepository, self).read(key)
+        try:
+            f = open(os.path.join(self.path, *key), 'rb')
+        except IOError as e:
+            raise RepositoryKeyError(key, str(e))
+        return self._read(f)
+
+    def _read(self, f):
+        with f:
+            while 1:
+                chunk = f.read(4096)
+                if not chunk:
+                    break
+                yield chunk
+
+    def write(self, key, iterable):
+        super(FileSystemRepository, self).write(key, iterable)
+        dirpath = list(key)[:-1]
+        dirpath.insert(0, self.path)
+        for i in xrange(len(dirpath)):
+            p = os.path.join(*dirpath[:i + 1])
+            print(p)
+            if not os.path.isdir(p):
+                os.mkdir(p)
+        with open(os.path.join(self.path, *key), 'wb') as f:
+            for chunk in iterable:
+                f.write(chunk)
 
     def __repr__(self):
         return '{0.__module__}.{0.__name__}({1!r})'.format(type(self),
--- tests/repository_test.py
+++ tests/repository_test.py
@@ -1,17 +1,68 @@
 from pytest import raises
 
-from libearth.repository import (FileNotFoundError, NotADirectoryError,
-                                 Repository)
+from libearth.repository import (FileNotFoundError, FileSystemRepository,
+                                 NotADirectoryError, Repository,
+                                 RepositoryKeyError)
+
+
+class RepositoryNotImplemented(Repository):
+
+    pass
+
+
+class RepositoryImplemented(Repository):
+
+    def read(self, key):
+        super(RepositoryImplemented, self).read(key)
+        return ''
+
+    def write(self, key, iterable):
+        super(RepositoryImplemented, self).write(key, iterable)
+
+
+def test_not_implemented_error():
+    r = RepositoryNotImplemented()
+    with raises(NotImplementedError):
+        r.read(['key'])
+    with raises(NotImplementedError):
+        r.write(['key'], '')
+    r2 = RepositoryImplemented()
+    assert r2.read(['key']) == ''
+    r2.write(['key'], '')
+
+
+def test_file_read(tmpdir):
+    f = FileSystemRepository(str(tmpdir))
+    with raises(RepositoryKeyError):
+        f.read([])
+    with raises(RepositoryKeyError):
+        f.read(['key'])
+    tmpdir.join('key').write('file content')
+    assert b''.join(f.read(['key'])) == b'file content'
+    with raises(RepositoryKeyError):
+        f.read(['dir', 'dir2', 'key'])
+    tmpdir.mkdir('dir').mkdir('dir2').join('key').write('file content')
+    assert b''.join(f.read(['dir', 'dir2', 'key'])) == b'file content'
+
+
+def test_file_write(tmpdir):
+    f = FileSystemRepository(str(tmpdir))
+    f.write(['key'], [b'file ', b'content'])
+    assert tmpdir.join('key').read() == 'file content'
+    f.write(['dir', 'dir2', 'key'], [b'deep ', b'file ', b'content'])
+    assert tmpdir.join('dir', 'dir2', 'key').read() == 'deep file content'
+    with raises(RepositoryKeyError):
+        f.write([], [b'file ', b'content'])
 
 
 def test_file_not_found(tmpdir):
     path = tmpdir.join('not-exist')
     with raises(FileNotFoundError):
-        Repository(str(path))
+        FileSystemRepository(str(path))
 
 
 def test_not_dir(tmpdir):
     path = tmpdir.join('not-dir.txt')
     path.write('')
     with raises(NotADirectoryError):
-        Repository(str(path))
+        FileSystemRepository(str(path))
