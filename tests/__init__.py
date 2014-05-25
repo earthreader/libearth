4a64384145ad8717ec2488bcfa1a8aae1354c218
Hong Minhee <minhee@dahlia.kr>
2014-05-26 04:20:14 +0900

    It seems to segfault on CPython 3.2+ as well
    See also: 8e06386d8f54570d04c883be506a0ac5e452e37d

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,9 +2,8 @@ import platform
 import sys
 
 
-# Workaround segmentation fault by cElementTree.fromstring() on Python 2.6/2.7
+# Workaround segmentation fault by cElementTree.fromstring() on CPython
 if platform.python_implementation() == 'CPython' and \
-   sys.version_info < (3, 0) and \
    platform.system() == 'Linux':
     from xml.etree import ElementTree
     sys.modules['xml.etree.cElementTree'] = ElementTree
