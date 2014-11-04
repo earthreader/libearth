1656432a2eece2afdc07c25b88a961837309dd6b
Hong Minhee <minhee@dahlia.kr>
2014-11-05 04:15:58 +0900

    Default subscription list

--- libearth/defaults.py
+++ libearth/defaults.py
@@ -9,8 +9,75 @@ try:
     import HTMLParser
 except ImportError:
     from html import parser as HTMLParser
+import io
+import re
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
 
-__all__ = 'BlogrollLinkParser',
+from .compat import UNICODE_BY_DEFAULT
+from .subscribe import SubscriptionList
+from .schema import complete, read
+
+__all__ = ('DEFAULT_BLOGROLL_URL', 'BlogrollLinkParser',
+           'get_default_subscriptions')
+
+
+# (:class:`str`) The url to blogroll OPML, or HTML page which links that.
+DEFAULT_BLOGROLL_URL = 'http://earthreader.org/'
+
+
+def get_default_subscriptions(blogroll_url=DEFAULT_BLOGROLL_URL):
+    """Suggest the default set of subscriptions.  The blogroll database
+    will be remotely downloaded from `Earth Reader website`__.
+
+    >>> subs = get_default_subscriptions()
+    >>> subs
+    <libearth.subscribe.SubscriptionList
+        'Feeds related to the Earth Reader project'
+        of Earth Reader Team <earthreader@librelist.com>>
+
+    :param blogroll_url: the url to download blogroll opml.
+                         default is the official website of earth reader
+    :type blogroll_url: :class:`str`
+    :returns: the default subscription list
+    :rtype: :class:`~.subscribe.SubscriptionList`
+
+    __ http://earthreader.org/
+
+    """
+    response = urllib2.urlopen(blogroll_url)
+    content_type = response.headers['content-type']
+    match = re.match(r'^\s*[^;/\s]+/[^;/\s]+', content_type)
+    mimetype = match and match.group(0)
+    if mimetype not in BlogrollLinkParser.SUPPORTED_TYPES:
+        parser = BlogrollLinkParser()
+        copy = io.StringIO() if UNICODE_BY_DEFAULT else io.BytesIO()
+        while 1:
+            chunk = response.read(1024)
+            if chunk:
+                parser.feed(chunk)
+                copy.write(chunk)
+            else:
+                break
+        pair = parser.get_link()
+        response.close()
+        if pair is None:
+            copy.seek(0)
+            response = copy
+        else:
+            copy.close()
+            url = urlparse.urljoin(response.url, pair[0])
+            response = urllib2.urlopen(url)
+    subscriptions = read(SubscriptionList, response)
+    complete(subscriptions)
+    response.close()
+    return subscriptions
 
 
 class BlogrollLinkParser(HTMLParser.HTMLParser):
--- tests/defaults_test.py
+++ tests/defaults_test.py
@@ -1,4 +1,9 @@
-from libearth.defaults import BlogrollLinkParser
+import datetime
+
+from libearth.defaults import BlogrollLinkParser, get_default_subscriptions
+from libearth.feed import Person
+from libearth.tz import utc
+from .conftest import MOCK_URLS
 
 
 HTML_LINKING_BLOGROLL = '''
@@ -37,3 +42,47 @@ def test_blogroll_link_parser():
     parser = BlogrollLinkParser()
     parser.feed(HTML_NOT_LINKING_BLOGROLL)
     assert parser.get_link() is None
+
+
+OPML = '''\
+<?xml version="1.0" encoding="utf-8"?>
+<opml version="2.0">
+<head>
+    <ownerName>Earth Reader Team</ownerName>
+    <ownerEmail>earthreader@librelist.com</ownerEmail>
+    <ownerId>http://earthreader.org/</ownerId>
+</head>
+<body>
+    <outline title="Earth Reader Blog"
+        htmlUrl="http://blog.earthreader.org/"
+        created="Tue, 29 Oct 2013 15:30:00 +0000"
+        xmlUrl="http://blog.earthreader.org/index.xml"
+        text="Earth Reader Blog"
+        type="rss" />
+</body>
+</opml>
+'''
+
+
+MOCK_URLS.update({
+    'http://blogroll.com/web/': (200, 'text/html', HTML_LINKING_BLOGROLL),
+    'http://blogroll.com/web/feeds.xml': (200, 'text/x-opml', OPML),
+})
+
+
+def test_get_default_subscriptions(fx_opener):
+    subs = get_default_subscriptions('http://blogroll.com/web/')
+    assert subs.owner == Person(
+        name='Earth Reader Team',
+        email='earthreader@librelist.com',
+        uri='http://earthreader.org/'
+    )
+    assert len(subs) == 1
+    sub = next(iter(subs))
+    assert sub.label == 'Earth Reader Blog'
+    assert sub.feed_uri == 'http://blog.earthreader.org/index.xml'
+    assert sub.alternate_uri == 'http://blog.earthreader.org/'
+    assert sub.created_at == datetime.datetime(2013, 10, 29, 15, 30, tzinfo=utc)
+    subs2 = get_default_subscriptions('http://blogroll.com/web/feeds.xml')
+    assert subs2.owner == subs.owner
+    assert frozenset(subs2) == frozenset(subs)
